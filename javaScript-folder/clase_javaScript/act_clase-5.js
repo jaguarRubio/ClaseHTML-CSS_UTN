4f0771b5bc843e61af8406b2044c344fe9ac3caa
bleeda097 <104221487+jaguarRubio@users.noreply.github.com>
2023-08-17 15:30:31 -0300

    act 4 sent

--- javaScript-folder/clase_javaScript/act_clase-5.js
+++ javaScript-folder/clase_javaScript/act_clase-5.js
@@ -1,6 +1,7 @@
 /** 1.- EVITAR QUE EL USUARIO INGRESE DATOS VACIOS.
  *  2.- EVITAR DUPLICIDAD DE DATOS POR EL DNI
  */
+// Power Point debajo de las consignas respondidas arriba
 
 var dataInput = () => {
     let firstN = document.getElementById("Firstname");
@@ -55,4 +56,28 @@ function DNI_detector(){
 };
     return flag;
 }
+
+// Power point
+/*PRACTICA - mostrar números mayores a 10
+Del siguiente array de números:
+let numeros = [15, 2, 35, 4, 5, 16, 7, 8, 10];
+Realizar un programa que imprima por consola únicamente los números mayores a 10
+*) Se imaginan que sentencia usar para recorrer los elementos? (no existe sólo una)
+PRACTICA - Array de autos
+En una agencia multimarca de autos tenemos un array con los autos a la venta estacionados en la playa:
+let autos = ["VW Up", "Renault Sandero", "VW Gol", "Fiat Uno"];
+Nos avisan que se vendió el Fiat Uno ubicado en el último lugar del estacionamiento. Entones tenés que eliminar el último elemento.
+Luego, entra un nuevo vehiculo (un "Ford Fiesta"), por lo que hay que agregarlo en el final de la lista de autos.
+PRACTICA - Array de autos
+Por último, se encontró un error en la lista, equivocados se habia dado de alta un VW Up cuando en realidad se trataba de un VW Fox. Entonces hay que modificar la posición número 0 de nuestra "playa de estacionamiento" por el modelo correcto de auto.
+Escribi el código necesario para cumplir con todos los requerimientos pedidos.
+*/
+
+//1.
+let numeros = [15, 2, 35, 4, 5, 16, 7, 8, 10];
+filterOver10 = numeros.filter((elm) => {
+    return elm > 10    
+})
+
+console.log(`Over 10: ${filterOver10}`);
 
--- javaScript-folder/clase_javaScript/clase-4.js
+++ javaScript-folder/clase_javaScript/clase-4.js
@@ -2,6 +2,7 @@
  * 1.-FILTRADO POR NOMBRE (COINCIDA)(SOLICITAR AL USUARIO EL NOMBRE O PALABRA A BUSCAR)
  * 2.- MOSTRAR EL PROMEDIO DE EDADES
  */
+// Power Point debajo de las consignas respondidas arriba
 const prompt = require("../node_modules/prompt-sync")(({sigint:true}))
 
 
@@ -90,7 +91,67 @@ promedio = reduc / caja.length
 
 console.log(promedio)
 
+//Había una actividad semejante en alguna parte
 mi_nombre = "Matias Sayago"
 mi_nombre = mi_nombre.replace("i","í")
 mi_nombre_con_diacriticos = mi_nombre.replace("Sa","Sá")
 console.log(mi_nombre_con_diacriticos);
+
+// Power point
+/*Crearemos la función suma() así como la rango() y utilizaremos las dos para realizar una suma de una lista de números.
+
+Nombre y apellido
+Desarrola una función que muestre por consola el primer apellido de una persona, considerando una estructura similar del objeto que ves en la diapositiva. Además, desarrolla una función similar que muestre solo el nombre.
+
+Edad par
+Desarrolla una funcion que tome un objeto como el tienes a al derecha como parámetro y retorne verdadero o falso si la edad es par. Deberás calcular la edad dentro de la función.
+*/
+
+//1.
+function suma1(set){
+    sum=0;
+    console.log(`sumandos: ${set.length}`)
+    for(let x=0 ; x<set.length ; x++){
+        sum += parseInt(set[x]);
+    }
+    return sum;
+}
+//NO CORRESPONDE >>
+datas = prompt("Input the numbers separated by empty space:");
+datas = datas.split(" "); // output:["2", "6", "8"]
+console.log(suma1(datas));
+//<<
+function rango(n){
+    caja=[]
+    for(let x=1; x<=n; x++){
+        caja.push(x)
+    }
+    return caja
+};
+data = prompt("input the number: ")
+console.log(suma1(rango(data)));
+
+//2.
+person = {
+    firstName:"Alan",
+    lastName:"Turing",
+    birthDay:1912,
+    DidPass:1954,
+}
+function nom(per){
+    return per.firstName;
+}
+function surnom(per){
+    return per.lastName;
+}
+console.log(nom(person));
+console.log(surnom(person));
+
+//3.
+function pair_oddAge(per){
+    years_old = person.DidPass - person.birthDay;
+    pair_odd = (years_old%2)==0 ? true : false; 
+    return pair_odd;
+}
+
+console.log(`is age pair?: ${pair_oddAge(person)} (${person.birthDay}-${person.DidPass})`);
